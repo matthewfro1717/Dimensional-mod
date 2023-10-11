6d699d27492a559bfd91adf6346c52912267a4d3
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-10-11 01:10:49 -0400

    Update sounds.lua

--- bin/mods/Dimensional mod/data/dimensional/sounds.lua
+++ bin/mods/Dimensional mod/data/dimensional/sounds.lua
@@ -1,5 +1,5 @@
 function onEvent(name, value1, value2)
-    if name == 'Play Animation' and value1 == 'shoot' then
-       PlaySound('gun_shot', 5)
+    if name == 'Play Animation' and value1 == 'shoot' then 
+      playSound('shoot', 5)
   end
 end
